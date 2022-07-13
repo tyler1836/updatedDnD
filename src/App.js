890af7da971632e807a6a0950fff80bdd43e3d1f
unknown <tyler.r.roll@gmail.com>
2022-07-13 08:58:56 -0600

    initial set up and testing functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {React, useState} from "react";
+import {fig} from "./skills/skills.js"
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 function App() {
+  const [level, setLevel] = useState(0)
+  let levels = level;
+  const levelUp = () => {
+    levels = levels + 1, 
+    setLevel(levels)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div>
+        <h1>{fig[0].name} {fig[0].power}</h1> 
+        {(level >= 5) ? 
+        <div><ul> <li>{fig[1].name}</li> <li>{fig[1].power}</li></ul>  </div> : <div></div> 
+      }
+        <button onClick={()=> {levelUp()}} disabled={(level == 50)}>{(level == 50) ? "Max Level" : "Level UP"} </button> <ProgressBar now={level} animated variant="success" striped />
+        {(level == 30) ?
+        <div className="secondClass"><img src="https://64.media.tumblr.com/b2711736d392c609fedcbd7983eb34fe/tumblr_inline_os6g4luO8H1qdq19t_640.jpg" alt="" srcset="" width={100} height={100}/><button>Choose monk?</button><button>Choose pugilist?</button><button>Choose warrior?</button></div>  :
+        <div></div>
+      }
+      </div>
+    </main>
   );
 }
 
---
+++ src/skills/skills.js
@@ -0,0 +1,41 @@
+export const fig = [
+    {
+        name: "double tap",
+        power: "2d6"
+    },
+    {
+        name: "Fist of Fury",
+        power: "3d6"
+    },
+    {
+        name: "Focused Breath",
+        healing: "1d12"
+    }, 
+    {
+        name: "Tempered Will",
+        trait: "Roll Advantage on attack moves for 2 turns"
+    },  
+    {
+        name: "Dragon Kick",
+        power: "4d8"
+    },
+    {
+        name: "Meditate",
+        heal: "2d12"
+    },
+    {
+        name: "Quadruple Tap",
+        power: "4d12"
+    },
+    {
+        name: "Buddah's Palm",
+        power: "4d20"
+    },
+    
+]
+
+export const arcanist = [
+    {
+
+    }
+]
