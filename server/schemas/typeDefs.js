9190392c065a152d508c4e1ec0f9b10530209ac0
unknown <tyler.r.roll@gmail.com>
2022-07-20 23:08:36 -0600

    rebuild server

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,76 @@
+const { gql } = require('apollo-server');
+module.exports = gql`
+type Character {
+  _id: ID!
+  name: String!
+  stats: [stat]
+  personalities: [personality]
+  equipments: [equipment]
+  experience: Int
+  race: String!
+  alignment: String!
+  background: String!
+  createdAt: String!
+  user: User
+}
+type Stat{
+    proficiency: Int 
+    inspiration: Int 
+    strength: Int
+    dexterity: Int
+    constitution: Int
+    intelligence: Int
+    wisdom: Int
+    charisma: Int
+    perception: Int
+    armor: Int 
+    initiative: Int 
+    speed: Int
+    health: Int 
+    level: Int 
+    character: Character
+}
+type Personality{
+    traits: String 
+    ideals: String 
+    bonds: String
+    flaws: String 
+    proficiencies: String 
+    languages: String 
+    character: Character
+}
+type Equipment{
+    name: String!
+    type: String!
+    definition: String!
+}
+type User {
+  _id: ID!
+  username: String!
+  password: String!
+  email: String!
+  token: String!
+  createdAt: String!
+  characters: [Character]
+  gamemaster: Gamemaster
+}
+type Gamemaster{
+    _id: ID!
+    username: String!
+    password: String!
+    email: String!
+    token: String!
+    createdAt: String!
+    users: [User]
+}
+type Auth{
+  token: ID!
+  user: User!
+}
+type Query {
+  me: User
+}
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+}`
