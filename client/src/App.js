b25844de6fef25f3fa093554800aece67c4b90e5
unknown <tyler.r.roll@gmail.com>
2022-07-31 15:27:21 -0600

    changing httplink uri to production one

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 const httpLink = createHttpLink({
-  uri: '/graphql'
+  uri: 'https://dndserverside.herokuapp.com/graphql'
 })
 const client = new ApolloClient({
   //concat with request link to graphql
